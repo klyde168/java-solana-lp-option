e5046b6a55af3d97c469be08676b3db816c3cb1d
klyde168 <klyde168@gmail.com>
2025-05-31 21:52:58 +0800

    first commit

---
+++ src/main/java/com/example/java_solana_lp_option/JavaSolanaLpOptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_solana_lp_option;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSolanaLpOptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSolanaLpOptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_solana_lp_option/JavaSolanaLpOptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_solana_lp_option;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSolanaLpOptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
