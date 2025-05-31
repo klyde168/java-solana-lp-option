9b3cd1bbe64f1ae4b9fee92c8012217de0d1884c
klyde168 <klyde168@gmail.com>
2025-05-31 22:01:08 +0800

    fix

--- src/main/java/com/example/java_solana_lp_option/JavaSolanaLpOptionApplication.java
+++ src/main/java/com/example/java_solana_lp_option/JavaSolanaLpOptionApplication.java
@@ -8,6 +8,7 @@ public class JavaSolanaLpOptionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSolanaLpOptionApplication.class, args);
+	System.out.println("hello world");
 	}
 
 }
